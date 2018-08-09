ce6d5ebde58994e89e9d0a11541bd08b4c8f2df5
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-09 11:26:19 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,6 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  puts rand(1..6).to_i
-  rand(1..6).to_i
+  my_array = [1, 2, 3, 4, 5, 6]
+  my_array.sample(1 + rand(my_array.count))
 end
